ff0273c5cbb237dc10d38a243f6556d323d79ee9
Jerry Yu <hweetty@gmail.com>
2015-04-05 21:24:06 -0400

    add watcard support

--- app.dstools.js
+++ app.dstools.js
@@ -1,7 +1,14 @@
 var express = require("express");
 var app = express();
 
-var presto = require("./lib/presto.js");
-app.get(presto.path, presto.func);
+var toolPaths = [
+	"./lib/presto.js", 	// Presto Card
+	"./lib/watcard.js",	// uWaterloo WatCard
+]
+
+toolPaths.forEach(function (path) {
+	var tool = require(path);
+	app.get(tool.path, tool.func);
+});
 
 app.listen(11100);
